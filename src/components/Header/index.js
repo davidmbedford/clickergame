7b4868956096ec64632a6207d73e612f1383e71d
davidmbedford <david.m.bedford1@gmail.com>
2019-07-17 17:29:47 -0400

    navbar made, may not be complete - revisit props when doing appJS

---
+++ src/components/Header/index.js
@@ -0,0 +1,14 @@
+    
+import React from "react";
+import "./styles.css";
+
+function Header() {
+  return (
+    <header className="header">
+      <h1>Test your connection to the force!</h1>
+      <h4>Start by clicking below - but be warned, do not click the same character twice.</h4>
+    </header>
+  )
+}
+
+export default Header;
