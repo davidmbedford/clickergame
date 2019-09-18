9649fffe0837940a72d6238cc688df504e36e082
davidmbedford <david.m.bedford1@gmail.com>
2019-09-17 23:12:20 -0400

    accidental typo change - committing to avoid flow issues

--- src/components/optionsobject.js
+++ src/components/optionsobject.js
@@ -8,66 +8,66 @@ const optionsList = [
     {
         "id": 2,
         "name": "Han Solo",
-        "image": "https://www.fanjackets.com/wp-content/uploads/2015/12/Star-Wars-Han-Solo-Leather-Vest-Jacket.png",
+        "image": "images/Han_solo.png",
         "isClicked": false
     },
     {
         "id": 3,
         "name": "Luke Skywalker",
-        "image": "https://www.toplessrobot.com/wp-content/uploads/2008/10/Episode_4_Luke_Skywalker_1.jpg",
+        "image": "images/Luke_Skywalker.jpg",
         "isClicked": false
     },
     {
         "id": 4,
         "name": "Princess Leia",
-        "image": "https://i.pinimg.com/originals/02/a7/13/02a713eb93793c186e21be4a527686e7.jpg",
+        "image": "images/Princess_Leia.jpg",
         "isClicked": false
     },
     {
         "id": 5,
         "name": "Lando Calrissian",
-        "image": "https://i.pinimg.com/originals/b2/56/09/b256094750a1b033abfa5ffc0c29772c.jpg",
+        "image": "images/Lando_Calrissian.jpg",
         "isClicked": false
     },
     {
         "id": 6,
         "name": "Darth Vader",
-        "image": "http://pm1.narvii.com/6345/cc44aafebd68d21b0e9c7aa22f8ecf8c45e969a3_00.jpg",
+        "image": "images/Darth_Vader.jpg",
         "isClicked": false
     },
     {
         "id": 7,
         "name": "Obi Wan Kenobi",
-        "image": "https://amp.businessinsider.com/images/50eba1fb6bb3f77312000002-750-563.jpg",
+        "image": "images/Obi_Wan_Kenobi.jpg",
         "isClicked": false
     }, {
         "id": 8,
         "name": "Mon Mothma",
-        "image": "https://vignette.wikia.nocookie.net/disney/images/5/53/Rogue_One_photography_10.png/revision/latest?cb=20161107143007",
+        "image": "images/Mon_Mothma.png",
         "isClicked": false
     },
     {
         "id": 9,
         "name": "Yoda",
-        "image": "https://media.wired.com/photos/595485615578bd7594c46409/master/pass/Yoda-featured1.jpg",
+        "image": "images/Yoda.jpg",
         "isClicked": false
     },
     {
         "id": 10,
         "name": "Darth Sidius",
-        "image": "https://pm1.narvii.com/7003/2d51b3fd31989ae21dbe4239f343cb45bfd9b889r1-1080-1920v2_hq.jpg",
+        "image": "images/Emperor_Sidious.png",
         "isClicked": false
     },
     {
         "id": 11,
         "name": "Admiral Ackbar",
-        "image": "https://vignette.wikia.nocookie.net/starwars/images/2/29/Admiral_Ackbar_RH.png/revision/latest?cb=20170907053204",
+        "image": "images/Admiral_Ackbar.png",
         "isClicked": false
     },
     {
         "id": 12,
         "name": "Jabba the Hutt",
-        "image": "https://vignette.wikia.nocookie.net/starwars/images/7/7f/Jabba_SWSB.png/revision/latest?cb=20160910034237",
+        "image": "images/Jabba.png",
         "isClicked": false
     }
 ]
